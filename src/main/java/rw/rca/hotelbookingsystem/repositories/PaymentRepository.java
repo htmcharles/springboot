af33029556df9a63ae0d825c8c430c8e33fa9846
htmcharles <hatumacharles1@gmail.com>
2025-04-24 20:30:09 +0200

    error1

--- src/main/java/rw/rca/hotelbookingsystem/repositories/PaymentRepository.java
+++ src/main/java/rw/rca/hotelbookingsystem/repositories/PaymentRepository.java
@@ -6,6 +6,7 @@ import rw.rca.hotelbookingsystem.models.Payment;
 
 import java.util.List;
 
+
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
     interface BookingRepository extends JpaRepository<Address.Booking, Long> {
         List<Address.Booking> findByUserId(Long userId);
